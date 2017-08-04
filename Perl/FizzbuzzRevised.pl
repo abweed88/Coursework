214ac051668e847000eb067c867931ff7b73c4c5
Anthony Weed <abweed88@gmail.com>
2017-08-04 12:58:10 -0500

    Update FizzbuzzRevised.pl

--- Perl/FizzbuzzRevised.pl
+++ Perl/FizzbuzzRevised.pl
@@ -8,15 +8,15 @@ use diagnostics;
 print "Welcome to the fizzbuzz\n";
 
 my $num = 1;
-while ($num < 100, ++$num) {
-if ($num % 15 == 0){
-print "fizzbuzz\n";}
-elsif ($num % 9 == 0){ print "void\n";
-next;}
-if ($num % 10 == 0) { print "void\n";
-}
-elsif ($num % 3 == 0) {
-print "fizz\n";}
-elsif ($num % 5 == 0) {
-print "buzz\n";}
+  while ($num < 100, ++$num) {
+      if ($num % 15 == 0){
+          print "fizzbuzz\n";}
+      elsif ($num % 9 == 0){ print "void\n";
+          next;}
+      if ($num % 10 == 0) { print "void\n";
+       }
+      elsif ($num % 3 == 0) {
+          print "fizz\n";}
+            elsif ($num % 5 == 0) {
+                print "buzz\n";}
 }
